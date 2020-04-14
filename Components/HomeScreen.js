15cf09ef03586e1c55f91351876d4ebdebd11cfa
flip <henches@gmail.com>
2020-04-14 12:56:46 +0200

    ProgressSymbol OK

--- Components/HomeScreen.js
+++ Components/HomeScreen.js
@@ -14,11 +14,6 @@ class HomeScreen extends React.Component {
         console.log("HOME SCREEN CONSTRUCTOR DEBUT !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!)")
         super();
 
-        this._animateProgress = this._animateProgress.bind(this)
-
-
-        this.state = {
-        }
 
         // console.log("HOME SCREEN CONSTRUCTOR : myProgressAnimForZone = ", myProgressAnimForZone )
         // console.log("HOME SCREEN CONSTRUCTOR : state = ", this.state )
@@ -46,7 +41,7 @@ class HomeScreen extends React.Component {
         const { navigation } = this.props;
         this.focusListener = navigation.addListener("didFocus", () => { 
             console.log("HOME SCREEN DIDFOCUS WORKS GREATTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTT")
-            this._animateProgress(true)
+
         });
 
     }
@@ -71,198 +66,31 @@ class HomeScreen extends React.Component {
     }
 
   
-   _animateProgress = (test) => {
-        console.log("_animateProgress ");
 
-        let pM = this.props.pM
-        if (pM == null) 
-            return
+    _onEndAnim01 =() => { this.pS0._animateProgress2() }
+    _onEndAnim02 =() => { this.pS0._animateProgress3() }
+    _onEndAnim03 =() => { this.pS0._closeAnimations() }
 
-        console.log("_animateProgress PM is not null ");
+    _onEndAnim11 =() => { this.pS1._animateProgress2() }
+    _onEndAnim12 =() => { this.pS1._animateProgress3() }
+    _onEndAnim13 =() => { this.pS2._animateProgress() }
 
+    _onEndAnim21 =() => { this.pS2._animateProgress2() }
+    _onEndAnim22 =() => { this.pS2._animateProgress3() }
+    _onEndAnim23 =() => { this.pS0._animateProgress() }
 
-        this.pS1._animateProgress()
-
-
-        if (this.props.navigation.state.params == null) // Il n'y a pas de paramètres de navigation : c'est la première fois qu'on affiche Homme Screen -> pas besoin d'animation
-            return
-
-        if (this.props.navigation.state.params.lastScreen == null) // Le paramètre LastScreen n' pas été positionné : erreur dans le code
-            return
-
-        if (this.props.navigation.state.params.lastScreen.localeCompare("SeriesResultsScreen") != 0) // On revient d'un screen différent de SeriesResultsScreen
-            return
-
-
-    }
-
-
-    _onEndAnim11 =() => {
-        this.pS1._animateProgress2()
-    }
-
-    _onEndAnim12 =() => {
-        this.pS1._animateProgress3()
-    }
-
-    _onEndAnim13 =() => {
-        this.pS2._animateProgress()
-    }
-
-    _onEndAnim21 =() => {
-        this.pS2._animateProgress2()
-    }
-
-    _onEndAnim22 =() => {
-        this.pS2._animateProgress3()
-    }
-
-    _onEndAnim23 =() => {
-        this.pS3._animateProgress()
-    }
-
-    _onEndAnim31 =() => {
-        this.pS3._animateProgress2()
-    }
-
-    _onEndAnim32 =() => {
-        this.pS3._animateProgress3()
-    }
-
-    _onEndAnim33 =() => {
-        this.pS4._animateProgress()
-    }
-
-    _onEndAnim41 =() => {
-        this.pS4._animateProgress2()
-    }
-
-    _onEndAnim42 =() => {
-        this.pS4._animateProgress3()
-    }
-
-    _onEndAnim43 =() => {
-        this.pS0._animateProgress()
-    }
-
-    _onEndAnim01 =() => {
-        this.pS0._animateProgress2()
-    }
-
-    _onEndAnim02 =() => {
-        this.pS0._animateProgress3()
-    }
-
-    _onEndAnim03 =() => { // End of animations
-        this.props.dispatch({ type: "QUERES_STATS-DISPLAYED" })   // positionne oldPoints = Point (puisque l'anmation a été réalisée)
-    }
-
-
-
-  
 
     render() {
-
-    //        console.log("Go Séries : Qthis.props = ", this.props)
-    //      console.log("Go Séries : QuestionStats List = ", this.props.QuestionStatsList)
-    //     <Emoji name='flushed' style={{ fontSize: 30 }}/>
-
-        // console.log("this.props = ", this.props)  
-        console.log("HOME SCREEN RENDER ")  
-        // console.log("this.props.pM = ", this.props.pM)  
-        // console.log("this.props.QuestionStatsList = ", this.props.QuestionStatsList)  
-        // console.log("GetMaxPointsForZone(this.state.pm, G_Monde) = ", GetMaxPointsForZone(this.state.pM, G_Monde))  
-        let maxPointsWorld = 0 // Valeurs par défaut dans le cas ou le render est fait avant que l'initialisation de QuestionsStatList et pM ne soit réalisée
-        let pointsWorld = 0
-        let oldPointsWorld = 0
-        let maxPointsEurope = 0 
-        let pointsEurope = 0
-        let oldPointsEurope = 0
-        let maxPointsAfrique = 0 
-        let pointsAfrique = 0
-        let oldPointsAfrique = 0
-        let maxPointsAmeriques = 0 
-        let pointsAmeriques = 0
-        let oldPointsAmeriques = 0
-        let maxPointsAsiePacif = 0 
-        let pointsAsiePacif = 0
-        let oldPointsAsiePacif = 0
-        if (this.props.pM != null) {
-            console.log("HOME SCREEN RENDER PM is not NULL")
-            let pM = this.props.pM
-            
-            maxPointsWorld = GetMaxPointsForZone(pM, G_Monde)
-            pointsWorld = GetPointsForZone(pM, G_Monde)
-            oldPointsWorld = GetOldPointsForZone(pM, G_Monde)
-
-            maxPointsEurope = GetMaxPointsForZone(pM, G_Europe)
-            pointsEurope = GetPointsForZone(pM, G_Europe)
-            oldPointsEurope = GetOldPointsForZone(pM, G_Europe)
-
-            maxPointsAfrique = GetMaxPointsForZone(pM, G_Afrique)
-            pointsAfrique = GetPointsForZone(pM, G_Afrique)
-            oldPointsAfrique = GetOldPointsForZone(pM, G_Afrique)
-
-            maxPointsAmeriques = GetMaxPointsForZone(pM, G_Ameriques)
-            pointsAmeriques = GetPointsForZone(pM, G_Ameriques)
-            oldPointsAmeriques = GetOldPointsForZone(pM, G_Ameriques)
-
-            maxPointsAsiePacif = GetMaxPointsForZone(pM, G_AsiePacif)
-            pointsAsiePacif = GetPointsForZone(pM, G_AsiePacif)
-            oldPointsAsiePacif = GetOldPointsForZone(pM, G_AsiePacif)
-
-            // Pour tester l'animation
-            pointsWorld = 45
-            oldPointsWorld = 25
-            pointsEurope = 12
-            oldPointsEurope = 10
-            pointsAfrique = 16
-            oldPointsAfrique = 16
-            pointsAmeriques = 20
-            oldPointsAmeriques = 18
-            pointsAsiePacif = 5
-            oldPointsAsiePacif = 3
-
-            this.pS0._initProgressAnimation(oldPointsEurope, maxPointsEurope)
-            /*
-            this.pS2._initProgressAnimation(oldPointsAfrique, maxPointsAfrique)
-            this.pS3._initProgressAnimation(oldPointsAmeriques, maxPointsAmeriques)
-            this.pS4._initProgressAnimation(oldPointsAsiePacif, maxPointsAsiePacif)
-            this.pS0._initProgressAnimation(oldPointsWorld, maxPointsWorld)
-            */
-
-        }
-
-
-        /* Pour tester l'animation
-                        <View style={{ flex: 2, flexDirection: 'column', justifyContent: 'center' }}>  
-                    <TouchableOpacity style={Gstyles.button}
-                            onPress={() => { this.pS1._animateProgress() }}>
-                            <Text style={Gstyles.button_text}>Appel vers le component 1</Text>
-                    </TouchableOpacity>
-                </View>
-
-        */
-       /*
-                       <ProgressSymbol myFlex={ 1 } zone={ "Europe" } points={ pointsEurope } oldPoints={ oldPointsEurope }  maxPoints={ maxPointsEurope }
-                            onEndAnim1={ this._onEndAnim11 } onEndAnim2={ this._onEndAnim12 } onEndAnim3={ this._onEndAnim13 } ref={ ProgressSymbol => { this.pS1 = ProgressSymbol }} />
-                <ProgressSymbol myFlex={ 1 } zone={ "Afrique" } points={ pointsAfrique } oldPoints={ oldPointsAfrique }  maxPoints={ maxPointsAfrique }
-                            onEndAnim1={ this._onEndAnim21 } onEndAnim2={ this._onEndAnim22 } onEndAnim3={ this._onEndAnim23 } ref={ ProgressSymbol => { this.pS2 = ProgressSymbol }} />
-                <ProgressSymbol myFlex={ 1 } zone={ "Ameriques" } points={ pointsAmeriques } oldPoints={ oldPointsAmeriques }  maxPoints={ maxPointsAmeriques }
-                            onEndAnim1={ this._onEndAnim31 } onEndAnim2={ this._onEndAnim32 } onEndAnim3={ this._onEndAnim33 } ref={ ProgressSymbol => { this.pS3 = ProgressSymbol }} />
-                <ProgressSymbol myFlex={ 1 } zone={ "AsiePacif" } points={ pointsAsiePacif } oldPoints={ oldPointsAsiePacif }  maxPoints={ maxPointsAsiePacif }
-                            onEndAnim1={ this._onEndAnim41 } onEndAnim2={ this._onEndAnim42 } onEndAnim3={ this._onEndAnim43 } ref={ ProgressSymbol => { this.pS4 = ProgressSymbol }} />
-
-       */
-
-       // onPress={() => { this._goStatView() }}>
-
         return (
             <View style={Gstyles.main_view}>
                 <View style={{ flex: 1, flexDirection: 'row', justifyContent: 'center', alignItems: 'center' }}>
                     <Text style={{ fontSize: 30, fontWeight: 'bold'}}>CAPITALES</Text>
                 </View>
-                <ProgressSymbol myFlex={ 1 } zone={ "MONDE" } points={ pointsWorld } oldPoints={ oldPointsWorld }  maxPoints={ maxPointsWorld }
+                <ProgressSymbol myFlex={ 1 } zone={ "Europe" } points={ 18 } oldPoints={ 1 }  maxPoints={ 45 }
+                            onEndAnim1={ this._onEndAnim11 } onEndAnim2={ this._onEndAnim12 } onEndAnim3={ this._onEndAnim13 } ref={ ProgressSymbol => { this.pS1 = ProgressSymbol }} />
+                <ProgressSymbol myFlex={ 1 } zone={ "Afrique" } points={ 50 } oldPoints={ 48 }  maxPoints={ 50 }
+                            onEndAnim1={ this._onEndAnim21 } onEndAnim2={ this._onEndAnim22 } onEndAnim3={ this._onEndAnim23 } ref={ ProgressSymbol => { this.pS2 = ProgressSymbol }} />
+                <ProgressSymbol myFlex={ 1 } zone={ "MONDE" } points={ 20 } oldPoints={ 17 }  maxPoints={ 40 }
                             onEndAnim1={ this._onEndAnim01 } onEndAnim2={ this._onEndAnim02 } onEndAnim3={ this._onEndAnim03 } ref={ ProgressSymbol => { this.pS0 = ProgressSymbol }} />
                 <View style={{ flex: 2, flexDirection: 'column', justifyContent: 'center' }}>   
                     <TouchableOpacity style={Gstyles.button}
@@ -272,7 +100,7 @@ class HomeScreen extends React.Component {
                 </View>
                 <View style={{ flex: 3, justifyContent: 'center', alignItems: 'center' }}> 
                     <TouchableOpacity style={Gstyles.button}
-                                onPress={() => { this.pS0._animateProgress() }}>
+                                onPress={() => { this.pS1._animateProgress() }}>
                                 <Text style={[Gstyles.button_text, { paddingLeft: 15, paddingLeft: 15,fontSize: 20, color:'white' }]}>Statistiques</Text>
                     </TouchableOpacity>
                 </View>
--- Components/ProgressSymbol.js
+++ Components/ProgressSymbol.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import { Text, View, Animated, TouchableOpacity, Easing, Bounce } from 'react-native'
-import { COLORS, Gstyles } from './Styles'
 
 
-const barHeight = 25
+const barHeight = 18
+progressDuration = 1000
 
 
 // Affiche la progression en cours pour une zone
@@ -17,157 +17,94 @@ export class ProgressSymbol extends React.Component {
         super();
 
         this.state = {
-            pBsizeX : new Animated.Value(0),
-            pBfadeProgress : new Animated.Value(0)
+            circleSize : new Animated.Value(1),
+            progress : new Animated.Value(0),
+            counter : 0
         }
     }
 
-    _initProgressAnimation = (oldPoints, maxPoints) => {
-        this.state.pBsizeX.setValue(0)
-        this.state.pBfadeProgress.setValue(0)
+    componentDidMount() {
+        console.log("ComponentDidmount")
+        this.setState({ counter: this.props.oldPoints })
+        this.state.progress.setValue(Math.max(this.props.oldPoints/this.props.maxPoints, 0.05))
     }
 
-    _animateProgress  = () => {
-        // console.log("progress Symbol : _animateProgress ")
-        // console.log("progress Symbol : _animateProgress Zone = ", this.props.zone)
-        // console.log("progress Symbol : _animateProgress points = ", this.props.points)
-        // console.log("progress Symbol : _animateProgress oldPoints = ", this.props.oldPoints)
-        // console.log("progress Symbol : _animateProgress maxPoints = ", this.props.maxPoints)
 
-
-
-        if (this.props.oldPoints == this.props.points) { // Pas d'animation si pas de d'augmentation de points
-            this.props.onEndAnim1()
-            return
-        }
-
-
-        Animated.parallel([
-            Animated.timing(this.state.pBsizeX, {
-                toValue: 1,
-                duration: 200, 
-                easing: Easing.linear
-            }),
-            Animated.timing(                  
-                this.state.pBfadeProgress,{
-                    toValue: 0.1,                   
-                    duration: 200,              
-            })]
-        ).start( () =>  {
-            Animated.parallel([
-                Animated.timing(this.state.pBsizeX, {
-                    toValue: 0,
-                    duration: 200, 
-                    easing: Easing.linear
-                }),
-                Animated.timing(                  
-                    this.state.pBfadeProgress,{
-                        toValue: 0.3,                   
-                        duration: 200,              
-                })]
-            ).start( () =>  {                
-               console.log("progress Symbol : fin _animateProgress")
-               this.props.onEndAnim1()
-            })
-        })
+    _tick = () => {
+        this.setState(prevState => ({
+            counter: prevState.counter + 1
+          }))
     }
+
+   _animateProgress  = () => {
+    console.log("progress Symbol : _animateProgress ")
+    Animated.timing(this.state.circleSize, {
+            toValue: 2,
+            duration: 500, 
+            easing: Easing.linear
+    }).start( () =>  {     
+        this._animateProgress2()
+    })
+}
+
     
     _animateProgress2  = () => {
-        return
         console.log("progress Symbol : _animateProgress2 ")
-    
-        if (this.props.oldPoints == this.props.points) { // Pas d'animation si pas de d'augmentation de points
-            this.props.onEndAnim2()
-            return
-        }
 
-        Animated.parallel([
-            Animated.timing(this.state.pBsizeX, {
+        this.setState({ counter: this.props.oldPoints })
+        this.interval = setInterval(() => this._tick(), progressDuration/(this.props.points-this.props.oldPoints+1));
+
+        Animated.timing(this.state.progress, {
                 toValue: this.props.points/this.props.maxPoints,
-                duration: 400, 
-                easing: Easing.bounce
-            }),
-            Animated.timing(                  
-                this.state.pBfadeProgress,{
-                    toValue: 0.6,                   
-                    duration: 400,              
-            })]
-        ).start( () => {
-            console.log("progress Symbol : fin _animateProgress2")
-            this.props.onEndAnim2()
+                duration: progressDuration, 
+                easing: Easing.linear
+        }).start( () =>  {                
+            clearInterval(this.interval);
+            this.setState({ counter: this.props.points })
+            this._animateProgress3()
         })
     }
 
     _animateProgress3  = () => {
         console.log("progress Symbol : _animateProgress3 ")
-    
-        if (this.props.oldPoints == this.props.points) { // Pas d'animation si pas de d'augmentation de points
-            this.props.onEndAnim3()
-            return
-        }
-
-        Animated.timing(                  
-            this.state.pBfadeProgress, {
-                toValue: 1,                   
-                duration: 400,              
-            }
-        ).start(() => {
-            console.log("progress Symbol = ", this.props.zone, " : fin _animateProgress3")
-            this.props.onEndAnim3()
-        });                        
+        Animated.timing(this.state.circleSize, {
+                toValue: 1,
+                duration: 500, 
+                easing: Easing.linear
+        }).start( () =>  {      
+            this.props.onEndAnim3()           
+        })
     }
 
+    _closeAnimations  = () => {
+        console.log("progress Symbol : closeAnimations")
+    }
 
     render() {
 
         const myFlex = this.props.myFlex // chaine de caractères
-
-        const points = this.props.points  // points : nombre de points actuels pour la zone
-        const oldPoints = this.props.oldPoints  // points : nombre de points actuels pour la zone
-        const maxPoints = this.props.maxPoints  // points : nombre de points actuels pour la zone
-
         const zone = this.props.zone // chaine de caractères
+        console.log("render ProgressSymbol oldPoints = ", this.props.oldPoints)
+        console.log("render ProgressSymbol points = ", this.props.points)
+        console.log("render ProgressSymbol maxPoints = ", this.props.maxPoints)
+        let justify = 'flex-end'
+        // if (this.props.oldPoints/this.props.maxPoints < 0.05) justify = 'flex-start'
 
-        //console.log("Zone = ", zone)
-        //console.log("points = ", points)
-        //console.log("oldPoints = ", oldPoints)
-        //console.log("maxPoints = ", maxPoints)
-
-
-
-        /*
-            <View style={{ flex: myFlex, flexDirection: 'column', justifyContent: 'center', alignItems: 'center', marginRight: '5%', marginLeft: '5%' }}>
-                <Text style={{ fontSize: 20 }}>{zone}</Text>
-                <View style={{ width:'100%', marginRight: '5%', marginLeft: '5%'}}>
-                    <View style={{ backgroundColor: 'aqua', borderRadius: 10, height: barHeight, width:"100%", alignSelf: 'center'}}>               
-                        <Animated.View style={{ backgroundColor: 'dodgerblue', borderRadius: 10, position: 'absolute', left: 0, right: 0, top: 0, bottom: 0,
-                                height: this.state.pBsizeY,  width: this.state.pBsizeX.interpolate({ inputRange: [0, 1], outputRange: ['0%', '100%'] }) }}>
-                            <Animated.View style={{ zIndex:2, position:'absolute', alignSelf: 'flex-end', 
-                                    opacity: this.state.pBfadeProgress.interpolate({ inputRange: [0, 0.30, 0.31, 1], outputRange: [1, 1, 0, 0] })  }}>
-                                <Text style={{ fontSize: 12, color: 'white' }}> { oldPoints } </Text>
-                            </Animated.View>
-                            <Animated.View style={{ zIndex:2, position:'absolute', alignSelf: 'flex-end', 
-                                    opacity: this.state.pBfadeProgress.interpolate({ inputRange: [0, 0.31, 0.32, 1], outputRange: [0, 0, 1, 1] })  }}>
-                                <Text style={{ fontSize: 14, color: 'white' }}> { points } </Text>
-                            </Animated.View>
-                        </Animated.View>    
-                    </View>      
-                    <View style={{ position: 'absolute', alignSelf: 'flex-end' }}> 
-                        <Text style={{ fontSize: 10, color: 'black', opacity: 0.5 }}> { maxPoints } </Text>
-                    </View>
-                </View>
-            </View>
-*/
 
         return(
-            <View style={{ flex: myFlex, flexDirection: 'column', justifyContent: 'center', alignItems: 'center', marginRight: '5%', marginLeft: '5%' }}>
-                <Text style={{ fontSize: 20 }}>{zone}</Text>
-                <Animated.View style={{ flexDirection: 'row', justifyContent: 'center', width:'100%', marginRight: '5%', marginLeft: '5%',
-                        transform: [{ scale: this.state.pBsizeX.interpolate({ inputRange: [0, 1], outputRange: [1, 3] }) }] }}>
-                    <View style={{ flexDirection: 'row', justifyContent: 'center', alignItem: 'center', backgroundColor: 'dodgerblue', borderRadius: '50%', height: barHeight, width: barHeight }}>
-                        <Text style={{ fontSize: 12, fontWeight: 'bold', color: 'white' }}> 4 </Text>
-                    </View>      
-                </Animated.View>
+            <View style={{ flex: myFlex, flexDirection: 'column', justifyContent: 'center', alignItems: 'flex-start', marginRight: '5%', marginLeft: '5%' }}>
+                <Text style={{ fontSize: 20 }}>{ zone }</Text>
+                <View style={{ flexDirection: 'row', justifyContent:'flex-start', alignItems: 'center', backgroundColor: 'lightskyblue', borderRadius: 10, height: barHeight, width: '100%' }}>
+                    <Text style={{ position: 'absolute', left: '95%', fontSize: 10, color: 'black' }}> { this.props.maxPoints } </Text>
+                    <Animated.View style={{ flexDirection: 'row', justifyContent: justify, backgroundColor: 'blue', borderRadius: 10, height: barHeight, 
+                            width: this.state.progress.interpolate({ inputRange: [0,1], outputRange: ["0%","100%"] }) }}>      
+                        <Animated.View style={{ transform: [{ scale: this.state.circleSize }] }}>
+                            <View style={{ flexDirection: 'row', justifyContent: 'center', alignItems: 'center', backgroundColor: 'blue', borderRadius: '50%', height: barHeight, width: barHeight, marginRight: '0.5%' }}>
+                                <Text style={{ fontSize: 11, fontWeight: 'bold', color: 'white' }}> { this.state.counter } </Text>
+                            </View>   
+                        </Animated.View>
+                    </Animated.View>
+                </View>
             </View>
       )
     }  
